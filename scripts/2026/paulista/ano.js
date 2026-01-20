2f156d68ad1f9434315b365143901fcd6e959e19
Erick Souza <erickbs2006@gmail.com>
2026-01-20 11:30:35 -0300

    mais escudos e jogos

--- scripts/2026/paulista/ano.js
+++ scripts/2026/paulista/ano.js
@@ -4,102 +4,128 @@ export default {
   jogos: [
     // ===== JANEIRO =====
     {
-      data: "2026-01-14",
-      horario: "21:30",
-      estadio: "Morumbi",
-      mandante: { id: "sao-paulo", nome: "São Paulo" },
-      visitante: { id: "ponte-preta", nome: "Ponte Preta" },
-      transmissao: "TV Globo / Premiere"
-    },
-    {
-      data: "2026-01-15",
-      horario: "20:00",
+      data: "2026-01-10",
+      horario: "16:00",
       estadio: "Vila Belmiro",
       mandante: { id: "santos", nome: "Santos" },
-      visitante: { id: "ituano", nome: "Ituano" },
-      transmissao: "Premiere"
+      visitante: { id: "novorizontino", nome: "Novorizontino" },
+      transmissao: null
     },
     {
-      data: "2026-01-16",
-      horario: "21:30",
+      data: "2026-01-10",
+      horario: "20:30",
+      estadio: "Canindé",
+      mandante: { id: "portuguesa", nome: "Portuguesa" },
+      visitante: { id: "palmeiras", nome: "Palmeiras" },
+      transmissao: null
+    },
+    {
+      data: "2026-01-11",
+      horario: "16:00",
+      estadio: "Neo Química Arena",
+      mandante: { id: "corinthians", nome: "Corinthians" },
+      visitante: { id: "ponte-preta", nome: "Ponte Preta" },
+      transmissao: null
+    },
+    {
+      data: "2026-01-11",
+      horario: "20:30",
+      estadio: "Campos Maia",
+      mandante: { id: "mirassol", nome: "Mirassol" },
+      visitante: { id: "sao-paulo", nome: "São Paulo" },
+      transmissao: null
+    },
+
+    // Rodada 2
+    {
+      data: "2026-01-14",
+      horario: "19:30",
+      estadio: "Allianz Parque",
+      mandante: { id: "palmeiras", nome: "Palmeiras" },
+      visitante: { id: "santos", nome: "Santos" },
+      transmissao: null
+    },
+
+    // Rodada 3
+    {
+      data: "2026-01-17",
+      horario: "20:30",
       estadio: "Allianz Parque",
       mandante: { id: "palmeiras", nome: "Palmeiras" },
       visitante: { id: "mirassol", nome: "Mirassol" },
-      transmissao: "Premiere"
+      transmissao: null
     },
     {
       data: "2026-01-18",
-      horario: "18:30",
+      horario: "16:00",
       estadio: "Neo Química Arena",
       mandante: { id: "corinthians", nome: "Corinthians" },
-      visitante: { id: "guarani", nome: "Guarani" },
-      transmissao: "TV Globo"
+      visitante: { id: "sao-paulo", nome: "São Paulo" },
+      transmissao: null
+    },
+
+    // Rodada 4
+    {
+      data: "2026-01-21",
+      horario: "19:30",
+      estadio: "Morenum",
+      mandante: { id: "sao-paulo", nome: "São Paulo" },
+      visitante: { id: "portuguesa", nome: "Portuguesa" },
+      transmissao: null
     },
     {
       data: "2026-01-22",
       horario: "19:30",
-      estadio: "Urbano Caldeira",
-      mandante: { id: "sao-paulo", nome: "São Paulo" },
+      estadio: "Vila Belmiro",
+      mandante: { id: "santos", nome: "Santos" },
       visitante: { id: "corinthians", nome: "Corinthians" },
-      transmissao: "Premiere"
+      transmissao: null
     },
 
-    // ===== FEVEREIRO =====
+    // Rodada 5
+    {
+      data: "2026-01-24",
+      horario: "18:30",
+      estadio: "Arena Barueri",
+      mandante: { id: "palmeiras", nome: "Palmeiras" },
+      visitante: { id: "sao-paulo", nome: "São Paulo" },
+      transmissao: null
+    },
     {
       data: "2026-01-25",
       horario: "16:00",
       estadio: "Vila Belmiro",
       mandante: { id: "santos", nome: "Santos" },
-      visitante: { id: "Bragantino", nome: "Red Bull Bragantino" },
-      transmissao: "Premiere"
+      visitante: { id: "red-bull-bragantino", nome: "Red Bull Bragantino" },
+      transmissao: null
     },
+
+    // ===== FEVEREIRO =====
     {
-      data: "2026-02-04",
-      horario: "21:30",
-      estadio: "Allianz Parque",
-      mandante: { id: "palmeiras", nome: "Palmeiras" },
-      visitante: { id: "novorizontino", nome: "Novorizontino" },
-      transmissao: "Premiere"
+      data: "2026-02-01",
+      horario: "20:30",
+      estadio: "Morumbi",
+      mandante: { id: "sao-paulo", nome: "São Paulo" },
+      visitante: { id: "santos", nome: "Santos" },
+      transmissao: null
     },
-    {
-      data: "2026-02-07",
-      horario: "18:30",
-      estadio: "Neo Química Arena",
-      mandante: { id: "corinthians", nome: "Corinthians" },
-      visitante: { id: "ituano", nome: "Ituano" },
-      transmissao: "Premiere"
-    },
+    
+
     {
       data: "2026-02-08",
       horario: "20:30",
-      estadio: "Allianz Parque",
+      estadio: "Neo Química Arena",
       mandante: { id: "corinthians", nome: "Corinthians" },
       visitante: { id: "palmeiras", nome: "Palmeiras" },
-      transmissao: "TV Globo"
+      transmissao: null
     },
     {
       data: "2026-02-15",
-      horario: "18:30",
-      estadio: "Morumbi",
-      mandante: { id: "sao-paulo", nome: "São Paulo" },
-      visitante: { id: "mirassol", nome: "Mirassol" },
-      transmissao: "Premiere"
-    },
-    {
-      data: "2026-02-22",
-      horario: "18:30",
-      estadio: "Neo Química Arena",
-      mandante: { id: "corinthians", nome: "Corinthians" },
-      visitante: { id: "ponte-preta", nome: "Ponte Preta" },
-      transmissao: "Premiere"
-    },
-    {
-      data: "2026-02-28",
-      horario: "16:00",
-      estadio: "Vila Belmiro",
-      mandante: { id: "santos", nome: "Santos" },
-      visitante: { id: "novorizontino", nome: "Novorizontino" },
-      transmissao: "TV Globo"
+      horario: "20:30",
+      estadio: "Moisés Lucarelli",
+      mandante: { id: "ponte-preta", nome: "Ponte Preta" },
+      visitante: { id: "sao-paulo", nome: "São Paulo" },
+      transmissao: null
     }
   ]
 };
